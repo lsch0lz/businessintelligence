da90fc5571a7e86807bd141a67f53874166cd74d
lukasscholz <lukas.scholz@hwr-berlin.de>
2022-04-01 11:38:51 +0200

    initial load

---
+++ hilfsfunktionen.py
@@ -0,0 +1,66 @@
+import mysql.connector
+from mysql.connector import errorcode
+import pandas as pd
+import numpy as np
+from IPython.display import display_html
+
+def create_database(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("Database created successfully")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+
+def create_server_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    return connection
+
+
+
+def create_db_connection(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    return connection
+
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        results = cursor.execute(query, multi=True)
+        for cur in results:
+            # print('cursor:', cur)
+            if cur.with_rows:
+                print('result:', cur.fetchall())
+        connection.commit()
+        print("Query successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+        
+
+def display_side_by_side(*args):
+    html_str=''
+    for df in args:
+        html_str+=df.to_html()
+    display_html(html_str.replace('table','table style="display:inline"'),raw=True)
